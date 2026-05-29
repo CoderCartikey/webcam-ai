518787809144237170a02ccee045ff2db4eb3799
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-05-29 23:26:41 +0530

    Still Phase 2: basicsr internal issue [not working with torchvision 0.17]

--- enhance.py
+++ enhance.py
@@ -1,23 +1,27 @@
 import cv2
 import torch
-import torch.nn.functional as F
 import numpy as np
+import sys
+sys.path.insert(0, r'C:\Users\ASUS\Real-ESRGN')
+from realesrgan import RealESRGANer
+from basicsr.archs.rrdbnet_arch import RRDBNet
 
 device = torch.device("cuda")
-print(f"Using: {torch.cuda.get_device_name(0)}")
+print(f"Using: {torch.cuda}")
 
-def enhance_frame(frame):
-    img = frame.astype(np.float32) / 255.0
-    tensor = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).to(device)
+model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
 
-    upscaled = F.interpolate(tensor, scale_factor=2, mode='bicubic', align_corners=False)
+upsampler = RealESRGNer(
+    scale=4,
+    model_path="weights/RealESRGAN_x4plus.pth",
+    model=model,
+    title=128,
+    title_pad=10,
+    pre_pad=0,
+    half=True
+)
 
-    blurred = F.avg_pool2d(upscaled, kernel_size=3, stride=1, padding=1)
-    sharpened = upscaled + 0.5 * (upscaled - blurred)
-    sharpened = torch.clamp(sharpened, 0, 1)
-
-    result = sharpened.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
-    return (result * 255).astype(np.uint8)
+print("Model loaded!")
 
 cap = cv2.VideoCapture(0)
 
@@ -26,13 +30,13 @@ while True:
     if not ret:
         break
 
-    enhanced = enhance_frame(frame)
+    output, _=upsampler.enchance(frame, outscale=2)
 
-    cv2.imshow("Original", frame)
-    cv2.imshow("Enhanced", enhanced)
+    cv2.imshow('original', frame)
+    cv2.imshow('Enhanced', output)
 
     if cv2.waitKey(1) == ord('q'):
         break
-
+    
 cap.release()
 cv2.destroyAllWindows()
